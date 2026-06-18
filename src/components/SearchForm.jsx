bc6eee67caf25731c1481fc86eb62b2f4ed6eb76
mr-mohammadi-sab <asf423796@gmail.com>
2026-06-17 23:55:34 -0700

    commit

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -15,7 +15,7 @@ export default function SearchForm() {
   return (
     <form className="flex sm:w-[95%] sm:border border-dark-border rounded-full" onSubmit={handleSubmit}>
             <input type="text" placeholder="Search.." value={search} onChange={(e)=>{setSearch(e.target.value)}} className=" py-2 pl-2  rounded-l-full focus:border border-blue-500 focus:outline-0 dark:text-white dark:bg-black sm:w-full hidden sm:inline-block"/>
-            <button className="md:py-2 md:px-3  rounded-full sm:rounded-l-none sm:dark:bg-dark-card dark:text-white cursor-pointer text-lg w-14 bg-bg-secondary"><IoSearch size={24}/></button>
+            <button className="md:py-2 md:px-3  rounded-full sm:rounded-l-none sm:dark:bg-dark-card dark:text-white cursor-pointer text-lg w-14 bg-bg-secondary"><IoSearch size={24}/></button>          
     </form>
   )
 }
